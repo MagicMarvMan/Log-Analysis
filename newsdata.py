3797bf77c989e8172e6cbeae589164a8d804b92e
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2017-12-29 13:03:09 +0100

    Improved visualizing features

--- newsdata.py
+++ newsdata.py
@@ -4,8 +4,7 @@ import os
 import db
 
 def clear_shell():
-	for x in range(10):
-		print(" ")
+	os.system("clear")
 
 conn = psycopg2.connect("dbname=news user=postgres password=postgres")
 cursor = conn.cursor()
@@ -13,17 +12,11 @@ cursor = conn.cursor()
 
 queries = {"articles":{"sql":"SELECT articles.title, count(*) AS views FROM articles INNER JOIN log ON log.path LIKE concat('%',articles.slug,'%') GROUP BY articles.title, log.path ORDER BY views DESC LIMIT 3","title":"What are the most popular articles?"}}
 
+clear_shell()
 
 def execute_visual_query(name):
-	print(" ")
-	print(" ")
-	print("==========================================")
-	print(queries[name]["title"])
-	print("==========================================")
-	print(" ")
-	print("Loading...")
+	print("Loading "+str(name)+"...")
 	que = db.query(queries[name]["sql"],cursor)
-	clear_shell()
 	print(" ")
 	print(" ")
 	print("==========================================")
@@ -32,8 +25,8 @@ def execute_visual_query(name):
 	print(" ")
 	for x in que:
 		print(str(x[0])+" - "+str(x[1]))
+	print(" ")
 
 
-clear_shell()
 
 execute_visual_query("articles")
