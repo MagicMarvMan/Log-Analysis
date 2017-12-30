9978a8f86eed1014c359f1f384670b79b81f54bd
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2017-12-30 09:13:51 +0100

    Fixed PEP8 issues

--- newsdata.py
+++ newsdata.py
@@ -11,20 +11,28 @@ def clear_shell():
 config = json.loads(open("config.json", "r").read())
 
 conn = psycopg2.connect(
-    "dbname="+config["database"]+\
-    " user="+config["username"]+\
+    "dbname="+config["database"] +
+    " user="+config["username"] +
     " password="+config["password"])
 cursor = conn.cursor()
 
 
 queries = {
     "articles": {
-        "sql": "SELECT articles.title, count(*) AS views FROM articles INNER JOIN log ON log.path LIKE concat('%',articles.slug,'%') WHERE log.status='200 OK' GROUP BY articles.title, log.path ORDER BY views DESC LIMIT 3",
+        "sql": "SELECT articles.title, count(*) AS views FROM articles"
+        " INNER JOIN log ON log.path LIKE concat('%',articles.slug,'%')"
+        " WHERE log.status='200 OK'"
+        " GROUP BY articles.title, log.path"
+        " ORDER BY views DESC LIMIT 3",
         "title": "What are the most popular articles?"
     },
 
     "authors": {
-        "sql": "SELECT authors.name, count(*) AS views FROM articles INNER JOIN authors ON articles.author = authors.id INNER JOIN log ON log.path LIKE concat('%',articles.slug,'%') WHERE log.status='200 OK' GROUP BY authors.name ORDER BY views DESC",
+        "sql": "SELECT authors.name, count(*) AS views FROM articles"
+        " INNER JOIN authors ON articles.author = authors.id"
+        " INNER JOIN log ON log.path LIKE concat('%',articles.slug,'%')"
+        " WHERE log.status='200 OK' GROUP BY authors.name "
+        "ORDER BY views DESC",
         "title": "Who are the most popular articles authors of all time?"
     },
 
